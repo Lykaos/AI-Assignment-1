87f5b27d3e6cf9f1d952f012f7f0f9e186f281e3
Dmitry Siniukov <siniukov@kth.se>
2018-01-22 19:11:54 +0100

    Kinematic point solved

--- WebSolution/script.js
+++ WebSolution/script.js
@@ -16,6 +16,8 @@ var gap_x;
 var gap_y;
 var unit_x;
 var unit_y;
+var marker_width = 9;
+var marker_height = 9;
 
 var	path = []; // Optimal path
 var acceleration;
@@ -26,13 +28,23 @@ function init() {
 	ctx = canvas.getContext('2d');
 	gap_x = canvas.width*gap_perc / 100;
 	gap_y = canvas.height*gap_perc / 100;
-	obstacles = [ map1.obstacle_1, map1.obstacle_2, map1.obstacle_3, map1.obstacle_4, map1.obstacle_5 ];
+	initMap(map1);
+}
+
+function initMap(newMap) {
+	map = newMap;
+	obstacles = [];
+	for (var i = 1; i < 11; i++) {
+		val = map["obstacle_"+i];
+		if (val == undefined) {
+			break;
+		}
+		obstacles.push(val);
+	}
+	//obstacles = [ map.obstacle_1, map.obstacle_2, map.obstacle_3, map.obstacle_4, map.obstacle_5 ];
 	vertices = [];
 	obst_edges = [];
 	
-	start_vertex_ind = vertices.length;
-	vertices.push(map1.pos_start);
-
 	for (var i = 0; i < obstacles.length; i++) {
 		
 		prev_obst_vert = obstacles[i][obstacles[i].length - 1];
@@ -58,11 +70,11 @@ function init() {
 		}
 	}
 	
-	prev_obst_vert = map1.bounding_polygon[map1.bounding_polygon.length - 1];
-	for (var i = 0; i < map1.bounding_polygon.length; i++) {
+	prev_obst_vert = map.bounding_polygon[map.bounding_polygon.length - 1];
+	for (var i = 0; i < map.bounding_polygon.length; i++) {
 		
-		x = map1.bounding_polygon[i][0];
-		y = map1.bounding_polygon[i][1];
+		x = map.bounding_polygon[i][0];
+		y = map.bounding_polygon[i][1];
 		if (x < min_x) {
 			min_x = x;
 		}
@@ -75,14 +87,15 @@ function init() {
 		if (y > max_y) {
 			max_y = y;
 		}
-		vertices.push(map1.bounding_polygon[i]);
-		obst_edges.push([prev_obst_vert, map1.bounding_polygon[i]]);
-		prev_obst_vert = map1.bounding_polygon[i];
+		vertices.push(map.bounding_polygon[i]);
+		obst_edges.push([prev_obst_vert, map.bounding_polygon[i]]);
+		prev_obst_vert = map.bounding_polygon[i];
 	}
 	
-
+	start_vertex_ind = vertices.length;
+	vertices.push(map.pos_start);
 	goal_vertex_ind = vertices.length;
-	vertices.push(map1.pos_goal);
+	vertices.push(map.pos_goal);
 	
 	unit_x = (canvas.width-2*gap_x)/(max_x-min_x);
 	unit_y = (canvas.height-2*gap_y)/(max_y-min_y);
@@ -122,23 +135,23 @@ function init() {
 		}
 	}
 	
-	// Test
-	//ctx.font = "30px Arial";
-	//ctx.fillText("Drawing test", 10, 50);
+	document.getElementById("info").innerHTML = 'v_max: '+map.vehicle_v_max+';  phi_max: '+map.vehicle_phi_max+';  omega_max: '+map.vehicle_omega_max+';  a_max: '+map.vehicle_a_max+';  L: '+map.vehicle_L;
 	
 	drawMap();
-	dijkstra(adj_matrix);
 }
 
 function drawMap() {
 	
+	ctx.save();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
 	// Draw the bounding polygon
 	ctx.beginPath();
-	last_vertex = map1.bounding_polygon[map1.bounding_polygon.length-1];
+	last_vertex = map.bounding_polygon[map.bounding_polygon.length-1];
 	last_vertex_norm = translatePoint(last_vertex);
 	ctx.moveTo(last_vertex_norm[0], last_vertex_norm[1]);
-	for (var i = 0; i < map1.bounding_polygon.length; i++) {
-		v = map1.bounding_polygon[i];
+	for (var i = 0; i < map.bounding_polygon.length; i++) {
+		v = map.bounding_polygon[i];
 		v_norm = translatePoint(v);
 		ctx.lineTo(v_norm[0], v_norm[1]);
 	}
@@ -160,16 +173,65 @@ function drawMap() {
 		ctx.fill();
 	}
 	
-	/*
+	// Draw the markers
+	
+	start_point = translatePoint(map.pos_start);
+	ctx.translate(start_point[0], start_point[1]);
+	rotateAngle = Math.atan2(map.vel_start[0], map.vel_start[1]);
+	ctx.rotate(rotateAngle);
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(-(marker_width/2), -(marker_height/2), marker_width, marker_height);
+	// Draw vector
+	ctx.strokeStyle = "#000000";
+	ctx.beginPath();
 	ctx.moveTo(0, 0);
-	ctx.lineTo(100,50);
-	ctx.lineTo(50, 100);
-	ctx.lineTo(0, 90);
-	*/
+	ctx.lineTo(0, -Math.sqrt(map.vel_start[0]*map.vel_start[0] +
+		map.vel_start[1]*map.vel_start[1])*unit_y);
+	ctx.stroke();
+	ctx.rotate(-rotateAngle);
+	ctx.translate(-start_point[0], -start_point[1]);
+	
+	goal_point = translatePoint(map.pos_goal);
+	ctx.translate(goal_point[0], goal_point[1]);
+	rotateAngle = Math.atan2(map.vel_goal[0], map.vel_goal[1]);
+	ctx.rotate(rotateAngle);
+	ctx.fillStyle = "#0000CD";
+	ctx.fillRect(-(marker_width/2), -(marker_height/2), marker_width, marker_height);
+	// Draw vector
+	ctx.strokeStyle = "#000000";
+	ctx.beginPath();
+	ctx.moveTo(0, 0);
+	ctx.lineTo(0, -Math.sqrt(map.vel_goal[0]*map.vel_goal[0] +
+		map.vel_goal[1]*map.vel_goal[1])*unit_y);
+	ctx.stroke();
+	ctx.rotate(-rotateAngle);
+	ctx.translate(-goal_point[0], -goal_point[1]);
+	
+	ctx.restore();
 }
 
 function buildPathKinematic() {
-	alert('Build path under kinematic point model');
+	drawMap();
+	ctx.save();
+	
+	var shortestPathInfo = shortestPath(adj_matrix, vertices.length, start_vertex_ind);
+	path = constructPath(shortestPathInfo, goal_vertex_ind);
+	//TODO: check for Nan
+	firstPoint = translatePoint(vertices[start_vertex_ind]);
+	ctx.strokeStyle = "#228B22";
+	ctx.moveTo(firstPoint[0], firstPoint[1]);
+	prev_point = vertices[start_vertex_ind];
+	t = 0;
+	for (var i = 0; i < path.length; i++) {
+		v = translatePoint(vertices[path[i]]);
+		ctx.lineTo(v[0], v[1]);
+		t += (dist(prev_point, vertices[path[i]]))/map.vehicle_v_max;
+		prev_point = vertices[i];
+	}
+	ctx.stroke();
+	ctx.font = "15px Arial";
+	ctx.fillText("Traveling time: " + t, 10, 20);
+	ctx.restore();
 }
 
 function buildPathDynamic() {
@@ -203,7 +265,6 @@ function buildPathKC() {
 	alert('Build path under kinematic car model');
 }
 
-
 function segmentsIntersect(p0, p1, p2, p3) {
 	p0_x=p0[0]; p0_y=p0[1]; p1_x=p1[0]; p1_y=p1[1]; p2_x=p2[0]; p2_y=p2[1]; p3_x=p3[0]; p3_y=p3[1];
     s1_x = p1_x - p0_x;
@@ -226,37 +287,47 @@ function translatePoint(p) {
 	return [x,y];
 }
 
-function dijkstra(adj_matrix) {
-	/* TODO: Fix this algorithm. We can use Dmitry's.
-	var unvisited_nodes = [];
-	var node_weights = []; // First element is the starting node, last element is the final node.
-	var node_paths = [];
-	var best_path = [];
-
-	for (i = 1; i < vertices.length; i++) {
-		unvisited_nodes.push(i);
-		node_weights.push(Number.MAX_VALUE);
-		node_paths.push([0]);
+// Taken from: http://mcc.id.au/2004/10/dijkstra.js
+function shortestPath(edges, numVertices, startVertex) {
+	var done = new Array(numVertices);
+	done[startVertex] = true;
+	var pathLengths = new Array(numVertices);
+	var predecessors = new Array(numVertices);
+	for (var i = 0; i < numVertices; i++) {
+		pathLengths[i] = edges[startVertex][i];
+		if (edges[startVertex][i] != Infinity) {
+			predecessors[i] = startVertex;
+		}
 	}
-
-	node_weights[0] = 0; // The starting node has weight 0.
-
-	for (i = 0; i < vertices.length; i++) {
-		for (j = 0; j < unvisited_nodes.length; j++) {
-			if (node_weights[unvisited_nodes[j]] > node_weights[i] + adj_matrix[i][unvisited_nodes[j]]) {
-				node_weights[unvisited_nodes[j]] = node_weights[i] + adj_matrix[i][unvisited_nodes[j]];
-				node_paths[unvisited_nodes[j]].push(i);
+	pathLengths[startVertex] = 0;
+	for (var i = 0; i < numVertices - 1; i++) {
+		var closest = -1;
+		var closestDistance = Infinity;
+		for (var j = 0; j < numVertices; j++) {
+			if (!done[j] && pathLengths[j] < closestDistance) {
+				closestDistance = pathLengths[j];
+				closest = j;
 			}
 		}
-		unvisited_nodes.shift();
-
-	} 
-	return node_paths; */
-
-	// No obstacles scenario
-	path.push(vertices.length);
-	return path;
+		done[closest] = true;
+		for (var j = 0; j < numVertices; j++) {
+			if (!done[j]) {
+				var possiblyCloserDistance = pathLengths[closest] + edges[closest][j];
+				if (possiblyCloserDistance < pathLengths[j]) {
+					pathLengths[j] = possiblyCloserDistance;
+					predecessors[j] = closest;
+				}
+			}
+		}
+	}
+	return { "startVertex": startVertex, "pathLengths": pathLengths, "predecessors": predecessors };
 }
 
-
-
+function constructPath(shortestPathInfo, endVertex) {
+	var path = [];
+	while (endVertex != shortestPathInfo.startVertex) {
+		path.unshift(endVertex);
+		endVertex = shortestPathInfo.predecessors[endVertex];
+	}
+	return path;
+}
